cc866b3d1c3123788f31ad57c067e99d3d3cc99b
Santosh <santosh.guruju@icloud.com>
2020-06-12 18:05:16 +0800

    CSS/JS Minification Demo Project Added

---
+++ WPBDemo/APP_Start/BundleConfig.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+
+namespace WPBDemo.APP_Start
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/demo").Include(
+              "~/Scripts/angular.js",
+              "~/Scripts/angular-resource.js",
+              "~/Scripts/angular-animate.js",
+              "~/Scripts/angular-sanitize.js",
+              "~/Scripts/angular-ui/ui-bootstrap-tpls.js"
+            ));
+
+            bundles.Add(new StyleBundle("~/content/css").Include(
+                        "~/Content/bootstrap.css",
+                        "~/Content/ui-bootstrap-csp.css"
+                      ));
+
+        }
+
+        protected void Application_Start(object sender, EventArgs e)
+        {
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+
+    }
+}
